f68a1b91660a9fc0920aa2703070c955f582d46b
Huyenne <nguyenkhanhhuyen051122@gmai.com>
2021-09-28 15:27:40 +0700

    hi

---
+++ metmoi/metmoi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metmoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
